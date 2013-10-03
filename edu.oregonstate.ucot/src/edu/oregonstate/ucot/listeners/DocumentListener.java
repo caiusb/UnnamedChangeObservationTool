9ebed8316e756c519b4a865543a8c9fa090bb282
Caius Brindescu <brind@illinois.edu>
2013-10-03 14:51:10 -0700

    Made the listener use the recorder to transmit events

--- edu.oregonstate.ucot/src/edu/oregonstate/ucot/listeners/DocumentListener.java
+++ edu.oregonstate.ucot/src/edu/oregonstate/ucot/listeners/DocumentListener.java
@@ -2,16 +2,34 @@ package edu.oregonstate.ucot.listeners;
 
 import org.eclipse.jface.text.DocumentEvent;
 import org.eclipse.jface.text.IDocumentListener;
+import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IPathEditorInput;
+import org.eclipse.ui.internal.UIPlugin;
 
+import edu.oregonstate.edu.recorders.Recorder;
+
+@SuppressWarnings("restriction")
 public class DocumentListener implements IDocumentListener {
+
+	private Recorder recorder;
+
+	public void setRecorder(Recorder recorder) {
+		this.recorder = recorder;
+	}
+
 	@Override
 	public void documentChanged(DocumentEvent event) {
-		System.out.println("Document changed " + event.getDocument().toString());
+		IEditorPart activeEditor = UIPlugin.getDefault().getWorkbench()
+				.getActiveWorkbenchWindow().getActivePage().getActiveEditor();
+		IEditorInput editorInput = activeEditor.getEditorInput();
+		IPathEditorInput input = (IPathEditorInput) editorInput;
+		String changedFile = input.getPath().toPortableString();
+		recorder.record(changedFile, event.fOffset, event.fOffset, event.fText);
 	}
 
 	@Override
 	public void documentAboutToBeChanged(DocumentEvent event) {
 		// TODO Auto-generated method stub
-		
 	}
 }
