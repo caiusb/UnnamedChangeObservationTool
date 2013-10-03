8b29f0bd1d52066f3f95e213e082868604e33630
Caius Brindescu <brind@illinois.edu>
2013-10-03 16:40:28 -0700

    More test thing

--- edu.oregonstate.ucot.tests/src/edu/oregonstate/ucot/listeners/DocumentListenerTest.java
+++ edu.oregonstate.ucot.tests/src/edu/oregonstate/ucot/listeners/DocumentListenerTest.java
@@ -1,35 +1,66 @@
 package edu.oregonstate.ucot.listeners;
 
+import static org.junit.Assert.fail;
+
+import java.io.File;
+
+import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IWorkspaceRoot;
 import org.eclipse.core.resources.ResourcesPlugin;
-import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.jface.text.IDocumentExtension;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.internal.UIPlugin;
+import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestName;
 
 public class DocumentListenerTest {
-	
+
+	@Rule
+	public TestName testName = new TestName();
+	private IWorkspaceRoot workspace = ResourcesPlugin.getWorkspace().getRoot();
+	private IProject project;
+	private static final String testFile = "testFile.txt";
+
 	@Before
-	public void setUp() {
-		IProject project = createNewEmptyProject();
-		setUpProject(project);
+	public void setUp() throws Exception {
+		project = createNewEmptyProject();
+		addProjectFile(project);
 	}
 
-	private IProject createNewEmptyProject() {
-		IWorkspaceRoot workspace = ResourcesPlugin.getWorkspace().getRoot();
+	@After
+	public void tearDown() throws Exception {
+		project.delete(true, new NullProgressMonitor());
+	}
+
+	private void addProjectFile(IProject project) throws Exception {
+		IFile file = project.getFile(testFile);
+		if (file.exists())
+			fail("Test file already exists");
+
+		file.create(null, false, new NullProgressMonitor());
+	}
+
+	private IProject createNewEmptyProject() throws Exception {
 		IProject project = workspace.getProject("TestProject");
-		if(project.exists()){}
-			//fail("Could not create test project");
-		
-		try {
-			project.create(new NullProgressMonitor());
-		} catch (CoreException e) {
-		}
-		
+		if (project.exists())
+			fail("Project already exists");
+
+		project.create(new NullProgressMonitor());
+		project.open(new NullProgressMonitor());
+
 		return project;
 	}
-	
-	private void setUpProject(IProject project) {
-		
+
+	@Test
+	@SuppressWarnings("restriction")
+	public void testSimpleFile() throws Exception {
+		File file = new File("TestProject/" + testFile);
+		UIPlugin.getDefault().getWorkbench().getActiveWorkbenchWindow()
+				.getActivePage().openEditor(null, "testFile");
 	}
 }
