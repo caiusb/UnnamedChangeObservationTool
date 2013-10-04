185c36209e88d2ec83f6366062696791b88afdda
Caius Brindescu <brind@illinois.edu>
2013-10-03 20:36:12 -0700

    Added open editor registration

--- edu.illinois.codingtracker.recording/src/edu/illinois/codingtracker/recording/Activator.java
+++ edu.illinois.codingtracker.recording/src/edu/illinois/codingtracker/recording/Activator.java
@@ -3,11 +3,28 @@
  */
 package edu.illinois.codingtracker.recording;
 
+import org.eclipse.core.resources.IFile;
 import org.eclipse.core.runtime.ILog;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Platform;
 import org.eclipse.core.runtime.Status;
+import org.eclipse.jface.text.IDocument;
+import org.eclipse.jface.text.ITextOperationTarget;
+import org.eclipse.jface.text.source.ISourceViewer;
+import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IEditorReference;
+import org.eclipse.ui.IFileEditorInput;
+import org.eclipse.ui.IPersistableElement;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.progress.UIJob;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+
+import edu.illinois.codingtracker.listeners.document.FileDocumentListener;
 
 /**
  * 
@@ -28,6 +45,41 @@ public class Activator implements BundleActivator {
 	 */
 	public void start(BundleContext bundleContext) throws Exception {
 		plugin= this;
+		
+		UIJob job = new UIJob("Loading listeners") {
+			
+			@Override
+			public IStatus runInUIThread(IProgressMonitor monitor) {
+				regiserListenersForOpenEditors();
+				return Status.OK_STATUS;
+			}
+		};
+		job.schedule();
+	}
+
+	private void regiserListenersForOpenEditors() {
+		IWorkbenchWindow activeWindow = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
+		IEditorReference[] editorReferences = activeWindow.getActivePage().getEditorReferences();
+		for (IEditorReference editorReference : editorReferences) {
+			IDocument document = getDocumentForEditor(editorReference);
+			try {
+				IEditorInput editorInput= editorReference.getEditorInput();
+				IFileEditorInput fileEditorInput= null;
+				if (editorInput instanceof IFileEditorInput) {
+					fileEditorInput= (IFileEditorInput) editorInput;
+				}
+				IFile editorFile= fileEditorInput.getFile();
+				document.addDocumentListener(new FileDocumentListener(editorFile));
+			} catch (PartInitException e) {
+			}
+		}
+	}
+
+	private IDocument getDocumentForEditor(IEditorReference editorReference) {
+		IEditorPart editorPart = editorReference.getEditor(true);
+		ISourceViewer sourceViewer = (ISourceViewer) editorPart.getAdapter(ITextOperationTarget.class);
+		IDocument document = sourceViewer.getDocument();
+		return document;
 	}
 
 	/*
